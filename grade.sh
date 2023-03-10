b07a06e91b91a31ad2e2f909620f936117694fc2
ababoescu <ababoescu@ucsd.edu>
2023-03-10 14:17:23 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -3,3 +3,12 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+cd student-submission
+
+if[[ -e ListExamples.java ]]
+then
+    echo "File Is Found!"
+    cd ..
+    javac ListExamples.java
+    
